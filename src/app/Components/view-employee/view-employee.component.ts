5123800e30addd28f20011645000d03720d55d07
Ajith-thushara <thusharat563@.com>
2024-04-17 19:42:52 +0530

    delete employee

--- src/app/Components/view-employee/view-employee.component.ts
+++ src/app/Components/view-employee/view-employee.component.ts
@@ -4,66 +4,115 @@ import { AddNewComponent } from 'src/app/PopUpDialog/add-new/add-new.component';
 import { UpdateItemComponent } from 'src/app/PopUpDialog/update-item/update-item.component';
 import { DataService } from 'src/app/Services/data.service';
 import { User } from 'src/app/models/user';
+import { AlertService } from 'src/app/validation/AlertService';
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+import { TDocumentDefinitions } from 'pdfmake/interfaces';
+(pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-view-employee',
   templateUrl: './view-employee.component.html',
-  styleUrls: ['./view-employee.component.scss']
+  styleUrls: ['./view-employee.component.scss'],
 })
 export class ViewEmployeeComponent {
-removeItem() {
-throw new Error('Method not implemented.');
-}
-filterTerm: string = '';
-empList: User[] = []; // Populate this array with your items
+  filterTerm: string = '';
+  empList: User[] = []; // Populate this array with your items
 
   get filteredEmpList(): any[] {
-    return this.empList.filter(data => {
+    return this.empList.filter((data) => {
       return data.empName.toLowerCase().includes(this.filterTerm.toLowerCase());
     });
   }
 
-  
   // this variable is get data from model
   userToModify: User = new User();
   // this variable determines wither we are in changing or creating new user
   creatingMode: boolean = true;
 
-  constructor(private dataService: DataService, private _dialog: MatDialog) {
-    this.getAllUsers();
+  constructor(
+    private dataService: DataService,
+    private alertService: AlertService,
+    private _dialog: MatDialog
+  ) {
+    this.getAllEmp();
   }
 
   //Get All Users
-  getAllUsers() {
-    this.dataService.getAll().subscribe((response: any) => {
-      this.empList = response.content; // Assuming response.content is the array of employees
-      console.log(this.empList);
-    }, error => {
-      console.error('Error getting users:', error);
-    });
+  getAllEmp() {
+    this.dataService.getAll().subscribe(
+      (response: any) => {
+        this.empList = response.content; // Assuming response.content is the array of employees
+        console.log(this.empList);
+      },
+      (error) => {
+        console.error('Error getting users:', error);
+      }
+    );
   }
 
-
-  openAddForm(){
+  openAddForm() {
     this._dialog.open(AddNewComponent);
   }
 
-  updateItem(empID: string) {
-    this.dataService.getById(empID).subscribe(data => {
-      console.log(data)
+  updateEmp(empID: string) {
+    this.dataService.getById(empID).subscribe((data) => {
+      console.log(data);
       this._dialog.open(UpdateItemComponent, {
         data: {
-          employee: data // Pass the data to the UpdateItemComponent
-        }
+          employee: data, // Pass the data to the UpdateItemComponent
+        },
       });
     });
   }
-  
-   //Create new Item
-   CreateItem(){
-    const newUser = {
-       empName : this.userToModify.empName,
-       empAddress : this.userToModify.empAddress,
-    };
+
+  //Create new Item
+  // CreateItem() {
+  //   const newUser = {
+  //     empName: this.userToModify.empName,
+  //     empAddress: this.userToModify.empAddress,
+  //   };
+  // }
+
+  async deleteEmp(empID: any) {
+    const confirmed = await this.alertService.showDeleteConfirmation();
+    if (confirmed) {
+      this.dataService.deleteEmployee(empID).subscribe(
+        () => {
+          this._dialog.closeAll();
+          //location.reload();
+          this.alertService
+            .showSuccess('Employee data delete successfully!')
+            .then(() => {
+              location.reload();
+            });
+        },
+
+        (error) => {
+          console.error('Failed to delete employee:', error);
+          // Display an error message here, e.g., using MatSnackBar
+        }
+      );
+    }
   }
+
+ 
+
+  printEmp() {
+    content: [
+      'pdfmake (since it\'s based on pdfkit) supports JPEG and PNG format',
+      'If no width/height/fit is provided, image original size will be used',
+      {
+        image: 'a.jpg',
+      },
+    ]
+  }
+  
+  
+  
+  
+
+  
+
+
 }
--- src/app/Services/data.service.ts
+++ src/app/Services/data.service.ts
@@ -1,32 +1,38 @@
 import { HttpClient } from '@angular/common/http';
+import { identifierName } from '@angular/compiler';
 import { Inject, Injectable } from '@angular/core';
 import { Observable, Observer } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
   private BASE_URL = 'http://localhost:8080/api/v1/employee/';
 
-  constructor(private http: HttpClient) { }
-  
+  constructor(private http: HttpClient) {}
+
   getAll(): Observable<any> {
     return this.http.get<any>(`${this.BASE_URL}getAllEmployees`);
   }
 
   saveEmployee(data: any): Observable<any> {
-    return this.http.post(`${this.BASE_URL}saveEmployee`, data, { responseType: 'text' });
+    return this.http.post(`${this.BASE_URL}saveEmployee`, data, {
+      responseType: 'text',
+    });
   }
 
-getById(id: string): Observable<any> {
-  return this.http.get<any>(`${this.BASE_URL}searchEmployee/${id}`);
-}
+  getById(id: string): Observable<any> {
+    return this.http.get<any>(`${this.BASE_URL}searchEmployee/${id}`);
+  }
 
+  updateEmployee(data: any): Observable<any> {
+    console.log(data);
+    return this.http.put<any>(`${this.BASE_URL}updateEmployee`, data, {
+      responseType: 'json',
+    });
+  }
 
-updateEmployee(data: any): Observable<any> {
-  console.log(data)
-  return this.http.put<any>(`${this.BASE_URL}updateEmployee`, data, { responseType: 'json' });
-}
-
-
+  deleteEmployee(id: any) {
+    return this.http.delete<any>(`${this.BASE_URL}deleteEmployee/${id}`);
+  }
 }
